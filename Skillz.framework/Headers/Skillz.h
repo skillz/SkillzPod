cf3853580819a48e0b1d51e4b106b288e1109d17
Greg Herlihy <gherlihy@skillz.com>
2015-11-24 17:46:48 -0500

    Update to 12.1.9

---
+++ Skillz.framework/Headers/Skillz.h
@@ -0,0 +1,17 @@
+//
+//  Skillz.h
+//  SkillzSDK-iOS
+//
+//  Copyright (c) 2014 Skillz. All rights reserved.
+//  http://skillz.com/
+//
+//  Version: 12.1.9
+
+#import <Skillz/SkillzInstance.h>
+#import <Skillz/Skillz+Turnbased.h>
+#import <Skillz/SKZRoundResults.h>
+#import <Skillz/SKZTurnBasedPlayer.h>
+#import <Skillz/SKZTurnBasedMatchInfo.h>
+#import <Skillz/SKZPlayer.h>
+#import <Skillz/SKZMatchInfo.h>
+
