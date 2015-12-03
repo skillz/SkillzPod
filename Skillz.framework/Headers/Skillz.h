501414d30ed9d0aeebb7014ed46a390d6076dd31
Tj <tfallon@mail.depaul.edu>
2015-12-02 19:14:39 -0800

    12.1.17 release.

--- Skillz.framework/Headers/Skillz.h
+++ Skillz.framework/Headers/Skillz.h
@@ -5,7 +5,7 @@
 //  Copyright (c) 2014 Skillz. All rights reserved.
 //  http://skillz.com/
 //
-//  Version: 12.1.12
+//  Version: 12.1.17
 
 #import <Skillz/SkillzInstance.h>
 #import <Skillz/Skillz+Turnbased.h>
