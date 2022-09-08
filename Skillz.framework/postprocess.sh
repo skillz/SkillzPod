318b7efec1c19ede56ebccd98ea535007cef0ec6
Skillz Inc <integrations@skillz.com>
2022-09-08 05:02:00 -0700

    Release version 28.0.120

--- Skillz.framework/postprocess.sh
+++ Skillz.framework/postprocess.sh
@@ -1,13 +1,16 @@
 /usr/bin/perl -x "$0"
 exit
 
-#!perl start here 
+#!perl start here
 use strict;
 
 $ENV{PATH} = $ENV{PATH} . ':/usr/libexec';
 
 my $BUILT_PRODUCTS_DIR = $ENV{'BUILT_PRODUCTS_DIR'};
 my $INFOPLIST_PATH = $ENV{'INFOPLIST_PATH'};
+my $CODE_SIGN_ENTITLEMENTS = $ENV{'CODE_SIGN_ENTITLEMENTS'};
+my $PRODUCT_BUNDLE_IDENTIFIER = $ENV{'PRODUCT_BUNDLE_IDENTIFIER'};
+my $TEAM_IDENTIFIER_PREFIX = $ENV{'TeamIdentifierPrefix'};
 
 # escape some troublesome path characters
 $BUILT_PRODUCTS_DIR =~ s!'!\'!g;
@@ -23,6 +26,16 @@ print "Running Skillz SDK post processing script" . "\n";
 print "Built Products Path: " . $BUILT_PRODUCTS_DIR . "\n";
 print "Info.plist Path: " . $INFOPLIST_PATH . "\n";
 
+# Get platform version, warn the developer if it is below the suggested Platform Version
+
+my $dtPlatformVersion = `PlistBuddy -c \'Print DTPlatformVersion\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
+if( $dtPlatformVersion < 12 ) {
+   print "warning: Skillz suggests Platform Version 12.0 or above. Your Platform Version is: " . $dtPlatformVersion ;
+}
+
+# add AppID (teamIdentifier.ProductBundleID)
+`PlistBuddy -c \'Add :AppID string \"${TEAM_IDENTIFIER_PREFIX}${PRODUCT_BUNDLE_IDENTIFIER}\"\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
+
 # Add Plist value to properly inform user of Location Request for iOS 8
 
 my $locationInUse = `PlistBuddy -c \'Print NSLocationWhenInUseUsageDescription\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
@@ -31,10 +44,25 @@ if (!length($locationInUse)) {
    `PlistBuddy -c \'Add :NSLocationWhenInUseUsageDescription string \"Due to legal requirements we require your location in games that can be played for cash.\"\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
 }
 
-my $bluetoothInUse = `PlistBuddy -c \'Print NSBluetoothPeripheralUsageDescription\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
+my $locationAlways = `PlistBuddy -c \'Print NSLocationAlwaysUsageDescription\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
 
-if (!length($bluetoothInUse)) {
-`PlistBuddy -c \'Add :NSBluetoothPeripheralUsageDescription string \"Skillz uses Bluetooth to determine eligibility to play in local tournaments.\"\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
+if (!length($locationAlways)) {
+`PlistBuddy -c \'Add :NSLocationAlwaysUsageDescription string \"Due to legal requirements we require your location in games that can be played for cash.\"\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
+}
+
+
+# Add Plist value to properly inform user of camera roll usage for iOS 11
+
+my $photoLibraryUsage = `PlistBuddy -c \'Print NSPhotoLibraryAddUsageDescription\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
+
+if (!length($photoLibraryUsage)) {
+   `PlistBuddy -c \'Add :NSPhotoLibraryAddUsageDescription string \"This allows you to save photos to your camera roll.\"\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
+}
+
+my $contactsInUse = `PlistBuddy -c \'Print NSContactsUsageDescription\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
+
+if (!length($contactsInUse)) {
+    `PlistBuddy -c \'Add :NSContactsUsageDescription string \"Your contacts will be uploaded to the Skillz servers so you can easily find friends to chat and play.\"\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
 }
 
 my $photosInUse = `PlistBuddy -c \'Print NSPhotoLibraryUsageDescription\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
@@ -43,10 +71,16 @@ if (!length($photosInUse)) {
 `PlistBuddy -c \'Add :NSPhotoLibraryUsageDescription string \"Skillz uses access to your photo album to customize your profile picture.\"\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
 }
 
-my $calendarInUse = `PlistBuddy -c \'Print NSCalendarsUsageDescription\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
+my $cameraInInUse = `PlistBuddy -c \'Print NSCameraUsageDescription\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
 
-if (!length($calendarInUse)) {
-`PlistBuddy -c \'Add :NSCalendarsUsageDescription string \"Skillz uses access to your calendar to personalize your ads.\"\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
+if (!length($cameraInInUse)) {
+`PlistBuddy -c \'Add :NSCameraUsageDescription string \"Skillz can access your camera to customize your profile picture.\"\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
+}
+
+my $idfaInUse = `PlistBuddy -c \'Print NSUserTrackingUsageDescription\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
+
+if (!length($idfaInUse)) {
+`PlistBuddy -c \'Add :NSUserTrackingUsageDescription string \"Your data allows for the optimization of the in-app ad experience for you.\"\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
 }
 
 # Add Plist value to respect view controller status bar appearance
@@ -57,11 +91,38 @@ if (!length($calendarInUse)) {
 `PlistBuddy -c \'Delete :UIRequiresFullScreen\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
 `PlistBuddy -c \'Add :UIRequiresFullScreen bool YES\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
 
+# Add Plist value to prevent limited photo permission prompt
+`PlistBuddy -c \'Delete :PHPhotoLibraryPreventAutomaticLimitedAccessAlert\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
+`PlistBuddy -c \'Add :PHPhotoLibraryPreventAutomaticLimitedAccessAlert bool YES\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
+
+# Add AdNetworks for SKAdNetwork support
+
+my $adNetworkIds = `PlistBuddy -c \'Print SKAdNetworkItems\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
+my $dylib = "$BUILT_PRODUCTS_DIR/" . $ENV{'FRAMEWORKS_FOLDER_PATH'} . "/Skillz.framework/";
+
+if (!length($adNetworkIds)) {
+    `PlistBuddy -c \'Add :SKAdNetworkItems array\' "${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH}"`;
+    `PlistBuddy -c \'Merge "$dylib/AdNetworks.plist" :SKAdNetworkItems:\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
+} else {
+    `PlistBuddy -c \'Merge "$dylib/AdNetworks.plist" :SKAdNetworkItems:\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
+}
+
+# Add Plist value to use Light theme
+my $UIUserInterfaceStyleExists = `PlistBuddy -c \'Print UIUserInterfaceStyle\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
+if (length($UIUserInterfaceStyleExists)) {
+    print "Note: Due to the Skillz theming engine, we currently do not support the automatic Dark Mode UI adjustments made by iOS, and are updating your Info.plist to reflect this.\n";
+    `PlistBuddy -c \'Delete :UIUserInterfaceStyle\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
+}
+`PlistBuddy -c \'Add :UIUserInterfaceStyle string Light\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
+
 # Add Custom URL Scheme unique to your game.
 my $bundleId = `PlistBuddy -c \"Print CFBundleIdentifier\" "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
+$bundleId =~ s/\R//g;
 my $customURLScheme = "skillz.gamelinks." . $bundleId;
-$customURLScheme =~ s/\R//g;
-print "Custom url scheme: $customURLScheme";
+print "Custom url scheme: $customURLScheme \n";
+
+my $customPaymentsURLScheme = $bundleId . ".skillz.gamelinks.payments";
+print "Custom payments url scheme: $customPaymentsURLScheme \n";
 
 my $bundleTypes = `PlistBuddy -c \'Print CFBundleURLTypes\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
 
@@ -70,42 +131,66 @@ if (!length($bundleTypes)) {
     `PlistBuddy -c \'Add :CFBundleURLTypes array\' "${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH}"`;
     `PlistBuddy -c \'Add :CFBundleURLTypes: dict\' "${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH}"`;
     `PlistBuddy -c \"Add :CFBundleURLTypes:0:CFBundleURLName string ${customURLScheme}\" \"${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH}\"`;
+    `PlistBuddy -c \"Add :CFBundleURLTypes:0:CFBundleURLName string ${customPaymentsURLScheme}\" \"${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH}\"`;
     `PlistBuddy -c \'Add :CFBundleURLTypes:0:CFBundleURLSchemes array\' "${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH}"`;
     `PlistBuddy -c \"Add :CFBundleURLTypes:0:CFBundleURLSchemes: string "${customURLScheme}"\" \"${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH}\"`;
+     `PlistBuddy -c \"Add :CFBundleURLTypes:0:CFBundleURLSchemes: string "${customPaymentsURLScheme}"\" \"${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH}\"`;
 } else {
-    print "CFBundleURLTypes exists, check if we should add Skillz. \n";
-	# Only add our custom scheme if it does not yet exist
-	my $customScheme = `grep --quiet "$customURLScheme" "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
+    sub add_scheme_if_needed
+    {
+        my $scheme = $_[0];
+        print "CFBundleURLTypes exists, check if we should add Skillz scheme: $scheme. \n";
+        # Only add our custom scheme if it does not yet exist
+        my $customScheme = `grep "$scheme" "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
 
-	if (!length($customScheme)) {
-        print "Skillz URL scheme not yet set \n";
-		my $temporaryPlistPath = `dirname "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH/urlScheme.plist"`;
+        if (!length($customScheme)) {
+            print "Skillz URL scheme $scheme not yet set \n";
+            my $temporaryPlistDirectory = `dirname "${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH}"`;
+            $temporaryPlistDirectory =~ s/\R//g;
+            my $tempPlistName = "/urlScheme.plist";
+            my $temporaryPlistPath = $temporaryPlistDirectory . $tempPlistName;
 
-		unlink "$temporaryPlistPath";
+            unlink "$temporaryPlistPath";
 
-		my $fileContents = "
-					<array>
-						<dict>
-							<key>CFBundleURLName</key>
-                                <string>${customURLScheme}</string>
-							<key>CFBundleURLSchemes</key>
-							<array>
-                                <string>${customURLScheme}</string>
-							</array>
-						</dict>
-					</array>";
+            my $fileContents = "
+                        <array>
+                            <dict>
+                                <key>CFBundleURLName</key>
+                                    <string>${scheme}</string>
+                                <key>CFBundleURLSchemes</key>
+                                <array>
+                                    <string>${scheme}</string>
+                                </array>
+                            </dict>
+                        </array>";
 
-        open(my $fh, '>', "$temporaryPlistPath");
-        print "$fh" . "$fileContents" . "\n";
-        print $fh $fileContents;
-        close $fh;
+            open(my $fh, '>', "$temporaryPlistPath");
+            print "$fh" . "$fileContents" . "\n";
+            print $fh $fileContents;
+            close $fh;
 
-        `PlistBuddy -c \'Merge $temporaryPlistPath :CFBundleURLTypes:\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
+            `PlistBuddy -c \'Merge "$temporaryPlistPath" :CFBundleURLTypes:\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
 
-        unlink "$temporaryPlistPath";
-    } else {
-        print 'Skillz URL scheme already set';
+            unlink "$temporaryPlistPath";
+
+        } else {
+            print "Skillz URL scheme $scheme already set \n";
+        }
     }
+    add_scheme_if_needed($customURLScheme);
+    add_scheme_if_needed($customPaymentsURLScheme);
+}
+
+# Add fetch background mode
+if (system("PlistBuddy -c \'Print :UIBackgroundModes\' " . "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH")) {
+    print "Adding background modes";
+
+    `PlistBuddy -c \'Add :UIBackgroundModes array\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
+}
+
+if (`PlistBuddy -c \'Print :UIBackgroundModes\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"` !~ /fetch/) {
+    print "Adding fetch background mode";
+    `PlistBuddy -c \'Add :UIBackgroundModes: string fetch\' "$BUILT_PRODUCTS_DIR/$INFOPLIST_PATH"`;
 }
 
 # Set up localization.
@@ -125,6 +210,23 @@ if (!length($localizations)) {
     `PlistBuddy -c \'Add :CFBundleLocalizations:8 string zh-Hans\' "${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH}"`;
 }
 
+my $skillzDict =  `PlistBuddy -c \'Print :skillzSDK:environment\'  "${BUILT_PRODUCTS_DIR}/${INFOPLIST_PATH}"`;
+
+if ($skillzDict =~ /Production/i && $ENV{'CONFIGURATION'} =~ /Debug/i) {
+  print "Debug Configuration is used for  Production build";
+  exit 1;
+}
+
+my $entitlements = `PlistBuddy -c \'Print :com.apple.developer.associated-domains\' "${CODE_SIGN_ENTITLEMENTS}"`;
+
+if (!length($entitlements)) {
+     `PlistBuddy -c \"Add :com.apple.developer.associated-domains array\" $CODE_SIGN_ENTITLEMENTS`;
+
+    `PlistBuddy -c \"Add :com.apple.developer.associated-domains:0 string \'applinks:dl.skillz.com\'\" "${CODE_SIGN_ENTITLEMENTS}"`;
+ 
+    `PlistBuddy -c \"Add :com.apple.developer.associated-domains:0 string \'webcredentials:skillz.com\'\" "${CODE_SIGN_ENTITLEMENTS}"`;
+}
+
 my $codeSignIdentity =  $ENV{'EXPANDED_CODE_SIGN_IDENTITY'};
 my $dylib = "$BUILT_PRODUCTS_DIR/" . $ENV{'FRAMEWORKS_FOLDER_PATH'} . "/Skillz.framework/Skillz";
 my $signingPath = "$BUILT_PRODUCTS_DIR/" . $ENV{'FRAMEWORKS_FOLDER_PATH'} . "/Skillz.framework";
@@ -138,13 +240,7 @@ if ( $ENV{'DEPLOYMENT_LOCATION'} eq "YES") {
     if (index($fileResult, "i386") != -1) {
         print "Exporting for release, remove unused archs. \n";
         my $tempfile = `mktemp -t skillz`;
-        my $removearmv7s = "";
-        
-        if ($ENV{'ARCHS'} !~ /armv7s/) {
-        	$removearmv7s = '-remove armv7s';
-        	print "Arch armv7s not found in app, removing from Skillz.framework\n";
-        }
-        `/usr/bin/lipo -output "$tempfile" -remove i386 -remove x86_64 $removearmv7s "$dylib"`;
+        `/usr/bin/lipo -output "$tempfile" -remove i386 -remove x86_64 "$dylib"`;
         `unlink "$dylib"`;
         `mv "$tempfile" "$dylib"`;
         print "Arch i386 found, removed \n";
