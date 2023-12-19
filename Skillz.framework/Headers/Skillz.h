2604f8590caf809b7c74c7a3886c95de22d26df0
Skillz Inc <integrations@skillz.com>
2023-12-18 16:10:40 -0800

    Release version 29.2.22

--- Skillz.framework/Headers/Skillz.h
+++ Skillz.framework/Headers/Skillz.h
@@ -5,14 +5,12 @@
 //  Copyright (c) 2014 Skillz. All rights reserved.
 //  http://skillz.com/
 //
-//  Version: 15.0.35
+//  Version: 29.2.22
 
 #import <Skillz/SkillzInstance.h>
 #import <Skillz/Skillz+Analytics.h>
-#import <Skillz/Skillz+Turnbased.h>
-#import <Skillz/SKZRoundResults.h>
-#import <Skillz/SKZTurnBasedPlayer.h>
-#import <Skillz/SKZTurnBasedMatchInfo.h>
+#import <Skillz/Skillz+SyncPlay.h>
+#import <Skillz/Skillz+Music.h>
+#import <Skillz/Skillz+Progression.h>
 #import <Skillz/SKZPlayer.h>
 #import <Skillz/SKZMatchInfo.h>
-
